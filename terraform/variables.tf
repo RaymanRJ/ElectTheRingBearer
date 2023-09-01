51e93a9474e5b8b10654ea79915403b9fed7b7f7
Rayman Jamal <RaymanRJ@Live.com>
2023-09-01 18:26:06 -0400

    Add Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+variable "domain_name" {
+  description = "Domain name for the website"
+  default     = "electtheringbearer.com"
+}
+
